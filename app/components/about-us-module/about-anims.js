a28b947dae956971abe7d74798f61e3650f7f60e
git-chad <tobimocc@gmail.com>
2023-10-10 11:13:34 -0300

    about us page

---
+++ app/components/about-us-module/about-anims.js
@@ -0,0 +1,18 @@
+// about-anims.js
+
+export const strokeAnimation = (pathRef) => {
+    const path = pathRef.current;
+
+    if (!path || path.tagName.toLowerCase() !== 'path') {
+      console.error('Invalid pathRef provided');
+      return;
+    }
+
+    const pathLength = path.getTotalLength();
+    const distance = window.scrollY;
+    const totalDistance = document.body.clientHeight - window.innerHeight;
+    const perc = distance / totalDistance;
+
+    path.style.strokeDasharray = `${pathLength}`;
+    path.style.strokeDashoffset = `${pathLength * (1 - perc)}`;
+};
