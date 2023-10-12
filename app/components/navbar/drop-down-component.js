24c0aba99c14388de093d168adc193e035120031
git-chad <tobimocc@gmail.com>
2023-10-12 18:01:56 -0300

    navbar products dropdown menu, custom scrollbar

---
+++ app/components/navbar/drop-down-component.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import "./navbar-component.scss"
+
+const Dropdown = ({ families, isActive }) => {
+  return (
+    <div className={`dropdown-content bg-white absolute flex space-x-16 text-black shadow-md p-8 top-6 ${isActive ? 'active' : ''}`}>
+      {families.map((family) => (
+        <div key={family.name} className="family-container mt-8 w-[90px]">
+          <Link href={`/products/${family.name}`} className="font-bold">
+            {family.name}
+          </Link>
+          <ul className="space-y-2 mt-4">
+            {family.products.map((product) => (
+              <li key={product} className="hover:underline underline-offset-2">
+                <Link href={`/products/${family.name}/${product}`}>             
+                  {product}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Dropdown;
--- app/components/navbar/navbar-component.js
+++ app/components/navbar/navbar-component.js
@@ -3,10 +3,33 @@ import React, { useEffect, useState, useRef } from "react";
 import RectangleButton from "../rectangle-button/rectangle-btn";
 import MenuComponent from "./button-component";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/s-logo.svg";
+import Dropdown from "./drop-down-component";
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [hideNavbar, setHideNavbar] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const productFamilies = [
+    {
+      name: 'KMM',
+      products: ['Altair', 'Atria', 'Fornax', 'Indus', 'Lynx', 'Pyxis']
+    },
+    {
+      name: 'GMG',
+      products: ['Bartop', 'Lyra', 'S-Lite 500']
+    },
+    {
+      name: 'ATM',
+      products: ['Alpha', 'Octans PMT', 'Polaris', 'Smart Box Cetus', 'Smart TCR Pegasus', 'Poli PMT']
+    },
+    {
+      name: 'TAS',
+      products: ['Axis PMT', 'Gamma PMT', 'Gamma II', 'Vega', 'Volans II']
+    },
+  ]
 
   useEffect(() => {
     const distanceCheck = () => {
@@ -35,21 +58,28 @@ const Navbar = () => {
   return (
     <>
       <nav
-  className={`z-30 lg:flex hidden justify-between items-center fixed top-0 left-0 w-screen h-[64px] bg-opacity-[0.1] backdrop-blur-md shadow-md ${
-    hideNavbar ? "translate-y-navbar" : ""
-  }`}
->
-        <Link href='/' className="logo ml-12">Symtron</Link>
+        className={`z-30 lg:flex hidden justify-between items-center fixed top-0 left-0 w-screen h-[64px] bg-opacity-[0.1] backdrop-blur-md ${
+          hideNavbar ? "translate-y-navbar" : ""
+        }`}
+      >
+        <Link href="/" className="logo ml-12">
+          <Image width={32} height={0} src={logo}></Image>
+        </Link>
         <div className="nav-items flex">
           <ul className="flex items-center justify-center space-x-8 px-8">
-            <li className="cursor-pointer">
-              <Link href='/products'>Products</Link>
+            <li 
+            className="cursor-pointer relative"
+            onMouseEnter={() => {setShowDropdown(true)}}
+            onMouseLeave={() => {setShowDropdown(false)}}
+            >
+              <Link href="/products">Products</Link>
+              {showDropdown && <Dropdown families={productFamilies} isActive={showDropdown}/>}
             </li>
             <li className="cursor-pointer">
-              <Link href='#'>Field service</Link>
+              <Link href="#">Field service</Link>
             </li>
             <li className="cursor-pointer">
-              <Link href='/about'>About us</Link>
+              <Link href="/about">About us</Link>
             </li>
           </ul>
 
